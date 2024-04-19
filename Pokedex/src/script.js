4d776e4413aa6bae06474c7a4fdc0ee200812640
Marcos Vinicius <marv.gfonseca@gmail.com>
2024-04-19 12:58:46 -0300

    Pokedex v2

--- Pokedex/src/script.js
+++ Pokedex/src/script.js
@@ -1,26 +1,34 @@
 const pokemonImg = document.querySelector("#pokemon");
 async function findPokemon() {
+  const pokemonName = document.getElementById("pokemon-name");
+  const pokemonImg = document.getElementById("pokemon");
+  const input = document.getElementById("input-pokemon");
   try {
     const pokemonId = document
       .getElementById("input-pokemon")
       .value.toLowerCase();
-    const pokemonImg = document.getElementById("pokemon");
-    const pokedex = document.getElementById("pokedex");
     const result = await fetch(
       `https://pokeapi.co/api/v2/pokemon/${pokemonId}`
     );
     if (!result.ok) {
-      throw new Error("Pokemon não encontrado");
+      throw new Error("Pokemon not found");
     }
     const data = await result.json();
     const Img =
       data.sprites.versions["generation-v"]["black-white"]["animated"][
         "front_default"
       ];
+    input.value = "";
     pokemonImg.src = Img;
     pokemonImg.style.display = "block";
-    pokedex.src = "../public/pokedex-2.png";
+    pokemonName.innerText = `#${data.id} - ${data.species.name}`;
+    pokemonName.style.color = "black";
   } catch (err) {
+    input.value = "";
+    pokemonImg.src = "";
+    pokemonImg.alt = "";
+    pokemonName.innerText = "Pokemon not found";
+    pokemonName.style.color = "red";
     console.log(err);
   }
 }
